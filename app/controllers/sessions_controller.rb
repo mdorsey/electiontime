916261edfa5baf77ed3c397ea5c54c81ef3c314c
Michael Dorsey <michael.william.dorsey@gmail.com>
2019-03-28 20:06:09 -0600

    adding 'remember me' checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
 
       # Log the user in, remember them, and redirect to the user's show page.
-      log_in user
-      remember user
-      redirect_to user
+      log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to(user)
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
