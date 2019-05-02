12dfc2f98abd261d8b9c2b375da0e551feb5ee21
Michael Dorsey <michael.william.dorsey@gmail.com>
2019-05-02 16:01:40 -0600

    working on moving Users into admin panel

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,33 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
-
+  before_action :admin_user,      only: [:index, :create, :new, :destroy]
+  before_action :set_user,        only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user,  only: [:edit, :update, :show]
+  before_action :correct_user,    only: [:edit, :update, :show]
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page]).order('last_name ASC')
   end
   
   def create
     @user = User.new(user_params)
+
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      flash[:info] = "User created successfully. Confirmation email has been sent to activate the account."
+      redirect_to users_url
     else
       render 'new'
     end
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    @user.destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def new
@@ -35,11 +35,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -49,6 +47,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :user_type_id, :password, :password_confirmation)
@@ -68,10 +70,4 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-
-    # # Confirms an admin user.
-    # def admin_user
-    #   redirect_to(root_url) unless current_user.user_type_id === UserType.find_by(name: 'Admin').id
-    # end
-
 end
