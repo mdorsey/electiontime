2b127c3384459b36055021a4b09647ed071bc805
Michael Dorsey <michael.william.dorsey@gmail.com>
2019-07-08 21:15:49 -0600

    updated participant model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,10 @@
 class Participant < ApplicationRecord
+
   belongs_to :user
-  validates :user_id, presence: true
+  
+  validates(:user_id, presence: true)
+  validates(:email, length: { maximum: 255 })
+  validates(:website, length: { maximum: 255 })
+  validates(:biography, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" })
+  
 end
