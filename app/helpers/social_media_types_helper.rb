794d06b93fe516ec4c7a01ae4084ea5d55d713a7
Michael Dorsey <michael.william.dorsey@gmail.com>
2019-07-31 21:37:36 -0600

    Added Social Media Profiles table 2

--- app/helpers/social_media_types_helper.rb
+++ app/helpers/social_media_types_helper.rb
@@ -1,2 +1,16 @@
 module SocialMediaTypesHelper
+
+  def display_social_media_type(social_media_type_id)
+    return SocialMediaType.find(social_media_type_id).name
+  end
+
+  def options_for_social_media_type
+    social_media_types = Array.new
+
+    SocialMediaType.all.order('name ASC').each do |s|
+      social_media_types.push([s.name, s.id])
+    end
+
+    return social_media_types
+  end
 end
