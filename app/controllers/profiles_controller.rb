ca7c1dab51039259668d1877e5ae3dd1b548d196
Michael Dorsey <michael.william.dorsey@gmail.com>
2020-04-13 09:51:08 -0600

    Cleaning up the Profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -23,74 +23,7 @@ class ProfilesController < ApplicationController
   end
 
   def update
-
-    params_to_update = profile_params
-
-    # Remove any previous pictures, if a new one was selected
-    if params[:participant][:picture].present?
-      @participant.picture.purge
-    end
-
-    # Add the Social Media Profile params
-    social_media_profiles_attributes = Array.new
-    SocialMediaType.all.order('name ASC').each do |t|
-
-      # Get the text box value (handle)
-      handle = params["smt_" + t.id.to_s]
-
-      # Check if the user already has a value set for this Type
-      profile = @participant.social_media_profiles.find { |smp| smp.social_media_type_id === t.id }
-
-      if profile && !handle.empty?
-        # Update the existing value
-        social_media_profiles_attributes << { id: profile.id, handle: handle, social_media_type_id: t.id }
-
-      elsif !handle.empty?
-        # Create a new value since one doesn't already exist
-        social_media_profiles_attributes << { handle: handle, social_media_type_id: t.id }
-
-      elsif profile
-        # Delete the existing value since the handle was removed
-        @smp_to_delete = SocialMediaProfile.find(profile.id)
-        @smp_to_delete.destroy
-      end
-    end
-
-    if social_media_profiles_attributes.any?
-      params_to_update[:social_media_profiles_attributes] = social_media_profiles_attributes
-    end
-
-    # Add the Survey Answer params
-    survey_answers_attributes = Array.new
-    @survey_questions.each do |question_id, question|
-
-      # Get the text box value (answer)
-      form_answer = params["sqa_" + question_id.to_s]
-
-      # Check if the user already has a value set
-      existing_answer = @participant.survey_answers.find { |sa| sa.survey_question_id === question_id }
-
-      if existing_answer && !form_answer.empty?
-        # Update the existing value
-        survey_answers_attributes << { id: existing_answer.id, survey_question_id: question_id, answer: form_answer }
-
-      elsif !form_answer.empty?
-        # Create a new value since one doesn't already exist
-        survey_answers_attributes << { survey_question_id: question_id, answer: form_answer }
-
-      elsif existing_answer
-        # Delete the existing value since the handle was removed
-        @sa_to_delete = SurveyAnswer.find(existing_answer.id)
-        @sa_to_delete.destroy
-      end
-    end
-
-    if survey_answers_attributes.any?
-      params_to_update[:survey_answers_attributes] = survey_answers_attributes
-    end
-
-    # Update all attributes
-    if @participant.update_attributes(params_to_update)
+    if @participant.update_profile(params, profile_params, @survey_questions)
       flash[:success] = "Profile updated"
       redirect_to profile_path(@participant)
     else
@@ -118,15 +51,13 @@ class ProfilesController < ApplicationController
 
       # Survey questions and answers
       @survey_questions = Hash.new
-      
-      if @participant.is_candidate
-        survey_type = SurveyType.find_by(name: "Candidate")
-      else 
-        survey_type = SurveyType.find_by(name: "Party")
-      end
-
       election = @participant.elections.find_by(active: true)
       if election
+        if @participant.is_candidate
+          survey_type = SurveyType.find_by(name: "Candidate")
+        else 
+          survey_type = SurveyType.find_by(name: "Party")
+        end
         survey = election.surveys.find_by(survey_type_id: survey_type.id)
         if survey
           SurveyQuestion.where(survey_id: survey.id).order(order: :asc).each do |q|
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -35,6 +35,77 @@ class Participant < ApplicationRecord
     "#{name} (#{party.name})"
   end
 
+  def update_profile(raw_params, permitted_params, survey_questions)
+
+    params_to_update = permitted_params
+
+    # Remove any previous pictures, if a new one was selected
+    if raw_params[:participant][:picture].present?
+      self.picture.purge
+    end
+
+    # Add the Social Media Profile params
+    social_media_profiles_attributes = Array.new
+    SocialMediaType.all.order(name: :asc).each do |t|
+
+      # Get the text box value (handle)
+      handle = raw_params["smt_" + t.id.to_s]
+
+      # Check if the user already has a value set for this Type
+      profile = self.social_media_profiles.find { |smp| smp.social_media_type_id === t.id }
+
+      if profile && !handle.empty?
+        # Update the existing value
+        social_media_profiles_attributes << { id: profile.id, handle: handle, social_media_type_id: t.id }
+
+      elsif !handle.empty?
+        # Create a new value since one doesn't already exist
+        social_media_profiles_attributes << { handle: handle, social_media_type_id: t.id }
+
+      elsif profile
+        # Delete the existing value since the handle was removed
+        @smp_to_delete = SocialMediaProfile.find(profile.id)
+        @smp_to_delete.destroy
+      end
+    end
+
+    if social_media_profiles_attributes.any?
+      params_to_update[:social_media_profiles_attributes] = social_media_profiles_attributes
+    end
+
+    # Add the Survey Answer params
+    survey_answers_attributes = Array.new
+    survey_questions.each do |question_id, question|
+
+      # Get the text box value (answer)
+      form_answer = raw_params["sqa_" + question_id.to_s]
+
+      # Check if the user already has a value set
+      existing_answer = self.survey_answers.find { |sa| sa.survey_question_id === question_id }
+
+      if existing_answer && !form_answer.empty?
+        # Update the existing value
+        survey_answers_attributes << { id: existing_answer.id, survey_question_id: question_id, answer: form_answer }
+
+      elsif !form_answer.empty?
+        # Create a new value since one doesn't already exist
+        survey_answers_attributes << { survey_question_id: question_id, answer: form_answer }
+
+      elsif existing_answer
+        # Delete the existing value since the handle was removed
+        @sa_to_delete = SurveyAnswer.find(existing_answer.id)
+        @sa_to_delete.destroy
+      end
+    end
+
+    if survey_answers_attributes.any?
+      params_to_update[:survey_answers_attributes] = survey_answers_attributes
+    end
+
+    # Update the participant and return the result
+    return self.update_attributes(params_to_update)
+  end
+
   private
 
     # Before deletion, check if the object is in use
