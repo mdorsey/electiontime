68157734848c609f3b0c69a72cc76e74683449aa
Michael Dorsey <michael.william.dorsey@gmail.com>
2020-07-24 13:36:36 -0600

    Added social media images

--- app/helpers/social_media_profiles_helper.rb
+++ app/helpers/social_media_profiles_helper.rb
@@ -1,14 +1,14 @@
 module SocialMediaProfilesHelper
 
-  def display_social_media_profile(social_media_profile_id)
+  def social_media_profile_link(social_media_profile_id)
 
-    profile_link = ''
+    link = ''
     social_media_profile = SocialMediaProfile.find(social_media_profile_id)
 
     if social_media_profile
-      profile_link = SocialMediaType.find(social_media_profile.social_media_type_id).website + social_media_profile.handle
+      link = SocialMediaType.find(social_media_profile.social_media_type_id).website + social_media_profile.handle
     end
 
-    return profile_link
+    return link
   end
 end
--- app/helpers/social_media_types_helper.rb
+++ app/helpers/social_media_types_helper.rb
@@ -13,4 +13,16 @@ module SocialMediaTypesHelper
 
     return social_media_types
   end
+
+  def get_social_media_type_icon(social_media_type_id)
+
+    icon_url = ''
+    icon = SocialMediaType.find(social_media_type_id).icon
+
+    unless icon.empty?
+      icon_url = 'social_media_icons/' + icon
+    end
+
+    return icon_url
+  end
 end
