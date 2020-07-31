2b7a1d9c74ed022abd5c37502ae41ffd92485edc
Michael Dorsey <michael.william.dorsey@gmail.com>
2020-07-31 10:15:16 -0600

    Arranged social media icons in correct size and order

--- app/helpers/social_media_profiles_helper.rb
+++ app/helpers/social_media_profiles_helper.rb
@@ -11,4 +11,19 @@ module SocialMediaProfilesHelper
 
     return link
   end
+
+  def get_social_media_profiles_in_alphabetical_order(participant_id)
+
+    participant = Participant.find(participant_id)
+    social_media_profiles = []
+
+    SocialMediaType.all.order(name: :asc).each do |t|
+      profile = participant.social_media_profiles.find { |smp| smp.social_media_type_id === t.id }
+      if profile
+        social_media_profiles << profile
+      end
+    end
+
+    return social_media_profiles
+  end
 end
