918524e241635eabfcbddc1335d2669a2e6a1594
Michael Dorsey <michael.william.dorsey@gmail.com>
2020-08-28 14:44:04 -0600

    Added comparison nav menu

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,13 @@
 // Toggle the hamburger menu open and close (using Bulma CSS)
-function toggleBurger() {
+function toggle_burger_menu() {
   var burger = $('.burger');
   var menu = $('.navbar-menu');
   burger.toggleClass('is-active');
   menu.toggleClass('is-active');
+}
+
+// Toggle the comparison pages navigation menu select box (using Bulma CSS)
+function toggle_comparison_nav_menu() {
+  var dropdown = $('.dropdown');
+  dropdown.toggleClass('is-active');
 }
