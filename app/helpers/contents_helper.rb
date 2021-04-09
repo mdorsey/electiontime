5a9d952b96e849e04f66adac059c697c289ffe02
Michael Dorsey <michael.william.dorsey@gmail.com>
2021-04-09 08:40:50 -0600

    Updated the feature to let users email candidates directly

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,17 +1,2 @@
 module ContentsHelper
-
-  def display_content(content_location_name)
-
-    display_content = ''
-
-    location = ContentLocation.find_by(name: content_location_name)
-    if location
-      content = Content.find_by(content_location_id: location.id)
-      if content
-        display_content = content.content
-      end
-    end
-
-    return display_content
-  end
 end
